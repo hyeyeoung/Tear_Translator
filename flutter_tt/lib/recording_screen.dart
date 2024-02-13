5ec86d9a14fedd506b752ce8140b995e3a8eb7df
hajiiiin <101108440+hajiiiin@users.noreply.github.com>
2024-02-13 16:46:37 +0900

    [ADD] listening page
    analysis screen 슬라이드

---
+++ flutter_tt/lib/recording_screen.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_tt/analysis_screen.dart';
+import 'package:lottie/lottie.dart';
+import 'dart:async';
+
+class RecordingScreen extends StatefulWidget {
+  const RecordingScreen({Key? key}) : super(key: key);
+
+  @override
+  _RecordingScreenState createState() => _RecordingScreenState();
+}
+
+class _RecordingScreenState extends State<RecordingScreen> {
+  final String imageRecordName = 'record.png';
+
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 2), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => AnalysisScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var screenHeight = MediaQuery.of(context).size.height;
+    var screenWidth = MediaQuery.of(context).size.width;
+
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: MediaQuery(
+        data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
+        child: Scaffold(
+          backgroundColor: Color.fromARGB(100, 41, 109, 182),
+          body: Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                SizedBox(height: screenHeight * 0.384375),
+                Container(
+                  child: Image.asset(
+                    imageRecordName,
+                    width: screenWidth * 0.616666,
+                    height: screenHeight * 0.0859375,
+                  ),
+                ),
+                SizedBox(height: MediaQuery.of(context).size.height * 0.0625),
+                Align(
+                  child: Text(
+                    "litening . . .",
+                    style: TextStyle(
+                      fontSize: screenWidth * (14 / 360),
+                      color: Color.fromRGBO(0, 51, 153, 0.6),
+                    ),
+                  ),
+                ),
+                SizedBox(height: MediaQuery.of(context).size.height * 0.0625),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
