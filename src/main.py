8f7dd883b3cd5865d1310af98200bbc9bf80879b
hy <rlagpdud0524@gmail.com>
2024-02-15 23:48:26 +0900

    모델 적용 api 작업 끝

--- src/main.py
+++ src/main.py
@@ -3,42 +3,37 @@
 ai 서비스 백엔드 파이썬
 
 '''
-from flask import Flask, request, url_for,Response, redirect, jsonify
+from flask import Flask, request, url_for,Response, redirect, jsonify,render_template
 from model.model import *
 import os
 # -----------------------------------------
 app = Flask(__name__)
-LOCATION = "./audio/"
+LOCATION = "src/audio/"
 ALLOWED_EXTENSIONS = {'mp3', 'WAV', 'wav', 'MP3'}
 # -----------------------------------------
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+@app.route('/')
+def test():
+    return render_template('test.html')
+        
 
-@app.route('/upload', methods=['POST'])
+@app.route('/upload', methods=['GET','POST'])
 def upload():
     try:
-        if request.files['file'] == '':
-            print("다시 업로드 해주세요")
-            return redirect(url_for(''))
-        file = request.files['file']
-        if file and allowed_file(file.filename):
-            path = os.path.join(LOCATION + file.filename)
-            file.save(path)
-            X_new = predict_data(path)
-            prediction = model.predict(X_new)
-            top_classes_indices = np.argsort(prediction)[::-1][:3]
-            result_dict = {"top_classes": []}
-            for i, class_index in enumerate(top_classes_indices):
-                class_info = {
-                    "rank": i+1,
-                    "class_name": classes[class_index],
-                    "probability": float(prediction[class_index])
-                }
-                result_dict["top_classes"].append(class_info)
-
-            return jsonify(result_dict)
+        if request.method == 'POST':        
+            file = request.files['files']
+            print("2")
+            if file and allowed_file(file.filename):
+                print("3")
+                path = os.path.join(LOCATION + file.filename)
+                file.save(path)
+                X_new = predict_data(path)
+                prediction = model.predict(X_new)
+                predicted_class = np.argmax(prediction)
+                return f"예측된 클래스 = {classes[predicted_class]}"
         return "g.."
     except Exception as e:
         print(e)
--- src/model/model.py
+++ src/model/model.py
@@ -3,7 +3,7 @@ import numpy as np
 from tensorflow.image import resize
 from tensorflow.keras.models import load_model
 
-model = load_model('./complete_model.h5') # 파일 경로에 한글이 있으면 안됨
+model = load_model('./src/model/complete_model.h5') # 파일 경로에 한글이 있으면 안됨
 
 classes = ['belly_pain','burping','discomfort','hungry','tired']
 
@@ -15,13 +15,13 @@ def predict_data(path, target_shape=(128, 128)):
     return np.array([mel_spectrogram])
 
 #샘플 데이터 경로
-path = "./dataset/belly_pain/69BDA5D6-0276-4462-9BF7-951799563728-1436936185-1.1-m-26-bp.wav"
-X_new = predict_data(path)
+# path = "./dataset/belly_pain/69BDA5D6-0276-4462-9BF7-951799563728-1436936185-1.1-m-26-bp.wav"
+# X_new = predict_data(path)
 
-# 새로운 데이터에 대한 예측 수행
-prediction = model.predict(X_new)
+# # 새로운 데이터에 대한 예측 수행
+# prediction = model.predict(X_new)
 
-predicted_class = np.argmax(prediction)
+# predicted_class = np.argmax(prediction)
 
-# 예측값 출력
-print(f"예측된 클래스 = {classes[predicted_class]}")
+# # 예측값 출력
+# print(f"예측된 클래스 = {classes[predicted_class]}")
