b78995ca4e95de657e228d5d716433f88deebba4
hajiiiin <101108440+hajiiiin@users.noreply.github.com>
2024-02-18 02:46:12 +0900

    음성 녹음 + 재생
    일단 실패..

--- flutter_tt/lib/record_screen.dart
+++ flutter_tt/lib/record_screen.dart
@@ -1,13 +1,12 @@
+import 'package:flutter/material.dart';
 import 'dart:io';
 import 'dart:typed_data';
-import 'package:flutter/material.dart';
 import 'package:flutter_sound/flutter_sound.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
 
-
 class RecordScreen extends StatefulWidget {
-  RecordScreen({this.title}) : super();
+  RecordScreen({Key? key, this.title}) : super(key: key);
   final String? title;
 
   @override
@@ -15,113 +14,97 @@ class RecordScreen extends StatefulWidget {
 }
 
 class _RecordScreenState extends State<RecordScreen> {
-  String viewTxt = "Record Player";
+  String viewTxt = "Recorde Player";
 
   late FlutterSoundRecorder myRecorder;
   late FlutterSoundPlayer myPlayer;
   bool check = false;
   bool playCheck = false;
 
-  GlobalKey<ScaffoldState> _key = GlobalKey<ScaffoldState>();
-
   @override
   void initState() {
-    super.initState();
-    initializeAudioSessions();
-  }
-
-  Future<void> initializeAudioSessions() async {
-    try {
+    Future.microtask(() async{
       myRecorder = FlutterSoundRecorder();
-      await myRecorder.openRecorder();
-      //myPlayer = FlutterSoundPlayer();
-      //await myPlayer.openAudioSession();
-    } catch (e) {
-      print('Error initializing audio sessions: $e');
-    }
+      await FlutterSoundRecorder().openRecorder();
+      myPlayer = FlutterSoundPlayer();
+    });
+    super.initState();
   }
 
   @override
-  void dispose() {
+  void dispose(){
     myRecorder.closeRecorder();
-    //myPlayer.closeRecorder();
+    myPlayer.closePlayer();
     super.dispose();
   }
 
-  Future<void> _recordFunction() async {
-    try {
-      PermissionStatus status = await Permission.microphone.request();
-      if (status != PermissionStatus.granted) {
-        throw RecordingPermissionException("Microphone permission not granted");
-      }
 
+  Future<void> _recodeFunc() async{
+    PermissionStatus status = await Permission.microphone.request();
+    if (status != PermissionStatus.granted) throw RecordingPermissionException("Microphone permission not granted");
+    setState(() {
+      viewTxt = "Recoding ~";
+    });
+    if(!check){
+      Directory tempDir = await getTemporaryDirectory();
+      File outputFile = File('${tempDir.path}/flutter_sound-tmp.aacADTS');
+      await myRecorder.startRecorder(toFile: outputFile.path,codec: Codec.aacADTS);
+      print("START");
       setState(() {
-        viewTxt = "Recording...";
+        check = !check;
       });
-
-      if (!check) {
-        Directory tempDir = await getTemporaryDirectory();
-        File outputFile = File('${tempDir.path}/flutter_sound-tmp.aacADTS');
-
-        await myRecorder.startRecorder(
-          codec: Codec.aacADTS,
-          toFile: outputFile.path,
-        );
-
-        print("START");
-        setState(() {
-          check = true;
-        });
-      } else {
-        print("STOP");
-        setState(() {
-          viewTxt = "Awaiting...";
-          check = false;
-        });
-        await myRecorder.stopRecorder();
-      }
-    } catch (e) {
-      print('Error recording: $e');
+      return;
     }
+    print("STOP");
+    setState(() {
+      check = !check;
+      viewTxt = "await...";
+    });
+    await myRecorder.stopRecorder();
+    return;
   }
 
-  Future<void> playRecording() async {
-    try {
-      if (!playCheck) {
-        Directory tempDir = await getTemporaryDirectory();
-        File inFile = File('${tempDir.path}/flutter_sound-tmp.aacADTS');
-
+  Future<void> playMyFile(BuildContext context) async{
+    if(!playCheck){
+      Directory tempDir = await getTemporaryDirectory();
+      File inFile = File('${tempDir.path}/flutter_sound-tmp.aacADTS');
+      try{
         Uint8List dataBuffer = await inFile.readAsBytes();
+        print("dataBuffer $dataBuffer");
         setState(() {
-          playCheck = true;
+          playCheck = !playCheck;
         });
-
-        await myPlayer.startPlayer(
-          codec: Codec.aacADTS,
-          fromDataBuffer: dataBuffer,
-          whenFinished: () {
-            print('Playback finished');
-            setState(() {});
-          },
-        );
-      } else {
-        await myPlayer.stopPlayer();
-        setState(() {
-          playCheck = false;
-        });
-        print("PLAY STOP!!");
+        await this.myPlayer.startPlayer(
+            fromDataBuffer: dataBuffer,
+            codec: Codec.aacADTS,
+            whenFinished: () {
+              print('Play finished');
+              setState(() {});
+            });
       }
-    } catch (e) {
-      print('Error playing recording: $e');
+      catch(e){
+        print(" NO Data");
+        ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text("NO DATA!!!!!!"),
+            )
+        );
+      }
+      return;
     }
+    await myPlayer.stopPlayer();
+    setState(() {
+      playCheck = !playCheck;
+    });
+    print("PLAY STOP!!");
+    return;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      key: _key,
       appBar: AppBar(
-        title: Text(widget.title ?? ''),
+        title: Text(widget.title!),
       ),
       body: Center(
         child: Column(
@@ -134,25 +117,25 @@ class _RecordScreenState extends State<RecordScreen> {
             Container(
               margin: EdgeInsets.all(20.0),
               child: FloatingActionButton(
-                onPressed: _recordFunction,
-                tooltip: 'Record',
-                child: check ? Icon(Icons.stop) : Icon(Icons.play_arrow),
+                onPressed: _recodeFunc,
+                tooltip: 'Increment',
+                child: check ? Icon(Icons.stop) :Icon(Icons.play_arrow),
               ),
             ),
             Container(
               padding: EdgeInsets.all(20.0),
               decoration: BoxDecoration(
-                border: Border.all(width: 2.0, color: Colors.grey[200]!),
-                borderRadius: BorderRadius.circular(15.0),
+                  border: Border.all(width: 2.0, color: Colors.grey[200]!),
+                  borderRadius: BorderRadius.circular(15.0)
               ),
               child: Column(
                 children: <Widget>[
-                  Text("Playback Controller\n(Recorded File)"),
+                  Text("Play Controller\n(Recorde File)"),
                   IconButton(
-                    icon: playCheck
-                        ? Icon(Icons.stop)
-                        : Icon(Icons.play_circle_filled),
-                    onPressed: playRecording,
+                    icon: playCheck ? Icon(Icons.stop) : Icon(Icons.play_circle_filled),
+                    onPressed: () async{
+                      await playMyFile(context);
+                    },
                   ),
                 ],
               ),
